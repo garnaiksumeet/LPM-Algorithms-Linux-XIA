83d35b152c5efb177ea88c7fd2dac32bb34b78c0
sumefsp <garnaiksumeet@gmail.com>
2015-10-03 19:58:23 +0530

    Added Buddy memory allocator files for Poptrie
    * Added buddy memory allocator files for Poptrie
    * Added xid_operations for Poptrie

---
+++ Poptrie/buddy.h
@@ -0,0 +1,38 @@
+/*_
+ * Copyright (c) 2014-2015 Hirochika Asai <asai@jar.jp>
+ * All rights reserved.
+ */
+
+#include "poptrie_xia.h"
+#include <stdlib.h>
+
+#ifndef _POPTRIE_BUDDY_H
+#define _POPTRIE_BUDDY_H
+
+/*
+ * Buddy system
+ */
+struct buddy {
+	/* Size of buddy system (# of blocks) */
+	int sz;
+	/* Size of each block */
+	int bsz;
+	/* Bitmap */
+	u8 *b;
+	/* Memory blocks */
+	void *blocks;
+	/* Level */
+	int level;
+	/* Heads */
+	u32 *buddy;
+};
+
+/* buddy.c */
+int buddy_init(struct buddy *, int, int, int);
+void buddy_release(struct buddy *);
+void * buddy_alloc(struct buddy *, int);
+int buddy_alloc2(struct buddy *, int);
+void buddy_free(struct buddy *, void *);
+void buddy_free2(struct buddy *, int);
+
+#endif
---
+++ Poptrie/xid_operations.c
@@ -0,0 +1,129 @@
+#include "poptrie_xia.h"
+
+/*
+ * Bitwise shift left an XID by shift bits and return that shifted XID
+ */
+static XID shift_left(XID id, int shift)
+{
+	int byte_s = 0;
+	int bit_s = 0;
+	int i,j;
+	XID tmp;
+
+	if (shift >= 160)
+		memset(&tmp, 0, 20);
+	else
+	{
+		byte_s = shift/8;
+		bit_s = shift%8;
+		if (0 == byte_s)
+		{
+			for (i=0;i<19;i++)
+				id.w[i] = (id.w[i] << bit_s)|(id.w[i+1] >> (8-bit_s));
+			id.w[i] = id.w[i] << bit_s;
+			tmp = id;
+			return tmp;
+		}
+		for (i=0;i<(20 - byte_s);i++)
+			id.w[i] = id.w[i+byte_s];
+		for (j=i;j<20;j++)
+			id.w[j] = (unsigned char) 0;
+		for (j=0;j<i;j++)
+			id.w[j] = (id.w[j] << bit_s)|(id.w[j+1] >> (8-bit_s));
+		tmp = id;
+	}
+	return tmp;
+}
+
+/*
+ * Bitwise shift right an XID by shift bits and return that shifted XID
+ */
+static XID shift_right(XID id, int shift)
+{
+	int byte_s = 0;
+	int bit_s = 0;
+	int i,j;
+	XID tmp;
+
+	if (shift >= 160)
+		memset(&tmp, 0, 20);
+	else
+	{
+		byte_s = shift/8;
+		bit_s = shift%8;
+		if (0 == byte_s)
+		{
+			for (i=19;i>0;i--)
+				id.w[i] = (id.w[i] >> bit_s)|(id.w[i-1] << (8-bit_s));
+			id.w[i] = id.w[i] >> bit_s;
+			tmp = id;
+			return tmp;
+		}
+		for (i=19;i>(byte_s - 1);i--)
+			id.w[i] = id.w[i-byte_s];
+		for (j=i;j>-1;j--)
+			id.w[j] = (unsigned char) 0;
+		for (j=19;j>i;j--)
+			id.w[j] = (id.w[j] >> bit_s)|(id.w[j-1] << (8-bit_s));
+		tmp = id;
+	}
+	return tmp;
+}
+
+/*
+ * Extract length bits starting at pos from data
+ */
+XID extractXID(int pos, int length, XID data)
+{
+	int i;
+	XID tmp;
+
+	// length = 0 is handled in the boolean expression and is short-circuit
+	// even though length of zero works in case of xids
+	tmp = shift_right(shift_left(data, pos), (160-length));
+	return tmp;
+}
+
+/*
+ * Remove "bits" bits from data
+ */
+XID removexid(int bits, XID data)
+{
+	int i;
+	XID tmp;
+
+	tmp = shift_right(shift_left(data, bits), bits);
+	return tmp;
+}
+
+/*
+ * Compare two xids i.e 160-bit addresses and returns 1, 0, -1 
+ * id1 > id2 : return 1
+ * id1 < id2 : return -1
+ * id1 = id2 : return 0
+ */
+int comparexid(const void *id1, const void *id2)
+{
+	int tmpresult;
+	XID **tmp1 = (XID **)id1;
+	XID **tmp2 = (XID **)id2;
+
+	tmpresult = memcmp(*tmp1, *tmp2, 20);
+
+	if (tmpresult < 0)
+		return -1;
+	else if (tmpresult > 0)
+		return 1;
+	else
+		return 0;
+}
+
+/*
+ * Convert and XID to uint32_t
+ */
+uint32_t XIDtounsigned(XID *bitpat)
+{
+	uint32_t tmp = bitpat->w[16]<<24 | bitpat->w[17]<<16 |
+				bitpat->w[18]<<8 | bitpat->w[19];
+	return tmp;
+}
